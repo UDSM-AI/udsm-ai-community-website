f4b9f52596c63a68aefa53dc2de8a7635cc40dc2
eddygulled <edgargulay69@gmail.com>
2024-02-02 00:38:37 +0300

    smallest chnk first

--- datadrive/models.py
+++ datadrive/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Count
 
 class MovementCategory(models.Model):
     name = models.CharField(max_length=50)
--- datadrive/views.py
+++ datadrive/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
+from django.db.models import Count
 # Create your views here.
 
 
 def data_drive_home(request):
-    return render(request, 'datadrive/index.html')
+    # Get the count of entries for each movement
+    movement_counts = MovementEntry.objects.values('movement').annotate(entry_count=Count('movement'))
+    movement_with_least_entries = Movement.objects.annotate(entry_count=Count('movemententry')).order_by('entry_count').first()
+
+    context = {
+        'action': movement_with_least_entries.name,
+        'description': movement_with_least_entries.description,
+    }
+
+    print(context)
+
+    return render(request, 'datadrive/index.html', context)
