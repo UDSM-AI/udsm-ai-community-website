dbc3f169701a922b8e884d83e86bfda5c2ba0233
eddygulled <edgargulay69@gmail.com>
2024-02-02 00:16:05 +0300

    display model name

--- datadrive/models.py
+++ datadrive/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-
-
 class MovementCategory(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     image_url = models.URLField()
+
+    def __str__(self):
+        return self.name
 
 
 class Movement(models.Model):
@@ -13,8 +14,14 @@ class Movement(models.Model):
     description = models.TextField()
     category = models.ForeignKey(MovementCategory, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class MovementEntry(models.Model):
     movement = models.ForeignKey(Movement, on_delete=models.CASCADE)
     audio_file = models.FileField(upload_to='audio/')
     notes = models.TextField()
+
+    def __str__(self):
+        return self.movement.name
