10c1199a7590101263bd710639b62afcfb74b746
eddygulled <edgargulay69@gmail.com>
2024-02-01 15:37:36 +0300

    datadrive models

--- datadrive/admin.py
+++ datadrive/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import MovementCategory, Movement, MovementEntry
 
-# Register your models here.
+@admin.register(MovementCategory)
+class MovementCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image_url')
+
+@admin.register(Movement)
+class MovementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'category')
+
+@admin.register(MovementEntry)
+class MovementEntryAdmin(admin.ModelAdmin):
+    list_display = ('movement', 'audio_file', 'notes')
--- datadrive/models.py
+++ datadrive/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class MovementCategory(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    image_url = models.URLField()
+
+
+class Movement(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(MovementCategory, on_delete=models.CASCADE)
+
+
+class MovementEntry(models.Model):
+    movement = models.ForeignKey(Movement, on_delete=models.CASCADE)
+    audio_file = models.FileField(upload_to='audio/')
+    notes = models.TextField()
